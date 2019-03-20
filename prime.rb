6829c0faa71cf115c9e837818faa3d17fa88dfb5
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 01:40:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,8 @@
 # Add  code here!
 def prime?(num)
-  if num === 0 || num === 1 || num === -1
+  if num <= 1
     false
   end
-    
-  counter = 2
-  while counter < num
-    if num % counter == 0
-      false
-    end
-      counter += 1
-  end
-  true
+  
+  
 end
