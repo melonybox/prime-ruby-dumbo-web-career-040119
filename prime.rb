299b3f8684b0aeb0e76a02aa1ee65d7653cb63b0
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 01:28:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,5 +5,7 @@ def prime?(num)
   elsif num % 2 == 0 || num % 3 == 0
     false
   counter = num
-  
+  while counter * counter <= num
+    if num % counter == 0 || num % (counter + 2) == 0
+      false
 end
