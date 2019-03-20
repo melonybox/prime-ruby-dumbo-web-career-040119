12187f3bf0813e974ddcade287c813e8725ff782
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 02:29:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  
+  (2..(num-1)).each do |x|
+    if num % x == 0
+      return false
+    end
+  end
+  return true
+end
